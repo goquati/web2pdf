95db02e5b039f40cf53f0f4d5709a340b5c95c72
Klaus Happacher <k.happacher@gmail.com>
2024-09-12 10:02:35 +0200

    disable chromium logs

--- main.sh
+++ main.sh
@@ -6,6 +6,6 @@ chromium-browser --headless --no-sandbox \
   --remote-debugging-address=0.0.0.0 \
   --remote-debugging-port=9222 \
   --enable-features=ConversionMeasurement,AttributionReportingCrossAppWeb \
-  about:blank > /dev/null &
+  about:blank > /dev/null 2>&1 &
 
 java -jar ./web2pdf.jar
